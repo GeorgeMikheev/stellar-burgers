d92a69b5de067d4c7f3fa968a6e06ca741400e3b
George Mikheev <GeorgeMikheev56@yandex.ru>
2024-09-10 15:18:56 +0300

    added new branch

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,19 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
+    <AppHeader /> 
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+      </Routes>
+    </BrowserRouter>   
   </div>
 );
 
