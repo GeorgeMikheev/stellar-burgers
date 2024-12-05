d457051c7c9bbb162cdf82855f6952bf2f0494d9
George Mikheev <GeorgeMikheev56@yandex.ru>
2024-12-05 21:42:45 +0500

    added routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,48 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
+import { Routes, Route } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route path='*' element={<NotFound404 />} />
+      <Route
+        path='/feed/:number'
+        element={<Modal children={<OrderInfo />} title='' onClose={() => {}} />}
+      />
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal children={<IngredientDetails />} title='' onClose={() => {}} />
+        }
+      />
+      <Route
+        path='/profile/orders/:number'
+        element={<Modal children={<OrderInfo />} title='' onClose={() => {}} />}
+      />
+    </Routes>
   </div>
 );
 
--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
+import { Link } from 'react-router-dom';
 import {
   BurgerIcon,
   ListIcon,
@@ -12,24 +13,24 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <>
+        <Link to='/' className={styles.link_position_last}>
           <BurgerIcon type={'primary'} />
           <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </>
-        <>
+        </Link>
+        <Link to='' className={styles.link_position_last}>
           <ListIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </>
+        </Link>
       </div>
       <div className={styles.logo}>
         <Logo className='' />
       </div>
-      <div className={styles.link_position_last}>
+      <Link to='/profile' className={styles.link_position_last}>
         <ProfileIcon type={'primary'} />
         <p className='text text_type_main-default ml-2'>
           {userName || 'Личный кабинет'}
         </p>
-      </div>
+      </Link>
     </nav>
   </header>
 );
